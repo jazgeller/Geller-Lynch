6ff1099e079997db0ac0a8004d9e1e68b0747503
jazgeller <103193497+jazgeller@users.noreply.github.com>
2023-03-22 17:39:26 -0300

    Terminado

--- Tema.hs
+++ Tema.hs
@@ -40,9 +40,9 @@ La función devuelve True si la etiqueta está en la lista de etiquetas, y False
 
 testing =
   [ nuevoT "Flowers" "Miley Cyrus" == Tem "Flowers" [] "Miley Cyrus",
-    nombreT "Flowers" ["Pop" "Funk"] "Miley Cyrus" == "Flowers",
-    datosT "Flowers" ["Pop" "Funk"] "Miley Cyrus" == "Miley Cyrus",
-    etiquetasT "Flowers" ["Pop" "Funk"] "Miley Cyrus" == ["Pop", "Funk"],
-    agregarT ["Disco"] "Flowers" ["Pop" "Funk"] "Miley Cyrus" == "Flowers" ["Pop" "Funk" "Disco"] "Miley Cyrus",
-    aplicaT ["Pop"] "Flowers" ["Pop" "Funk"] "Miley Cyrus" == True
+    nombreT (Tem "Flowers" ["Pop", "Funk"] "Miley Cyrus") == "Flowers",
+    datosT (Tem "Flowers" ["Pop", "Funk"] "Miley Cyrus") == "Miley Cyrus",
+    etiquetasT (Tem "Flowers" ["Pop", "Funk"] "Miley Cyrus") == ["Pop", "Funk"],
+    agregarT "Disco" (Tem "Flowers" ["Pop", "Funk"] "Miley Cyrus") == (Tem "Flowers" ["Disco", "Pop", "Funk"] "Miley Cyrus"),
+    aplicaT "Pop" (Tem "Flowers" ["Pop", "Funk"] "Miley Cyrus") == True
   ]
